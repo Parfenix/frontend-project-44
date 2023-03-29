1ae8cc7de55c9682ec11103863032db13410ae33
Denis Parfenov <Parfenix@gmail.com>
2023-03-29 11:26:57 +0300

    performed the check using the eslint

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
-import readline from 'readline-sync'
+import readline from 'readline-sync';
 
 const greeting = () => {
-   console.log('Welcome to the Brain Games!');
-   const name = readline.question('May I have your name? ');
-   console.log(`Hello, ${name}!`);
-   return name;
-}; 
+  console.log('Welcome to the Brain Games!');
+  const username = readline.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+};
 
 export default greeting;
